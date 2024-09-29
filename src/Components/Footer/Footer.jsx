175ee979e820b0a79d30f2753cd9906e93e3750f
Md Qaishar Zaki <109577871+md-qaishar-zaki@users.noreply.github.com>
2024-09-29 02:48:07 +0000

    .

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,24 +3,30 @@ import './Footer.css'
 
 export default function Footer() {
     return (
-        <footer className="footer pt-4 bg-gray-50">
-            <div className="container mx-auto px-4 footer__newslatter">
+        <footer className="footer pt-8 bg-gray-50">
+            <div className="container mx-auto px-4">
                 <div className="flex flex-wrap">
-                    <div className="w-full lg:w-1/3 md:w-1/2 sm:w-3/4 ">
+
+                    {/* About Section */}
+                    <div className="w-full lg:w-1/3 md:w-1/2 sm:w-full mb-8">
                         <div className="footer__about">
                             <div className="footer__logo mb-4">
                                 <a href="./index.html">
                                     {/* <img src="img/logo.png" alt="Logo" className="w-32" /> */}
-                                    <h6>Logo</h6>
+                                    <h6 className="text-2xl font-bold">Logo</h6>
                                 </a>
                             </div>
-                            <p className="mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt cilisis.</p>
+                            <p className="text-gray-500">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
                         </div>
                     </div>
-                    <div className="w-full lg:w-1/6 md:w-1/3 sm:w-1/2 ">
+
+                    {/* Quick Links */}
+                    <div className="w-full lg:w-1/6 md:w-1/3 sm:w-1/2 mb-8">
                         <div className="footer__widget">
                             <h6 className="text-lg font-semibold mb-4">Quick Links</h6>
-                            <ul className="space-y-2">
+                            <ul className="space-y-2 text-gray-600">
                                 <li><a href="#" className="hover:underline">About</a></li>
                                 <li><a href="#" className="hover:underline">Blogs</a></li>
                                 <li><a href="#" className="hover:underline">Contact</a></li>
@@ -28,10 +34,12 @@ export default function Footer() {
                             </ul>
                         </div>
                     </div>
-                    <div className="w-full lg:w-1/6 md:w-1/3 sm:w-1/2 ">
+
+                    {/* Account Links */}
+                    <div className="w-full lg:w-1/6 md:w-1/3 sm:w-1/2 mb-8">
                         <div className="footer__widget">
                             <h6 className="text-lg font-semibold mb-4">Account</h6>
-                            <ul className="space-y-2">
+                            <ul className="space-y-2 text-gray-600">
                                 <li><a href="#" className="hover:underline">My Account</a></li>
                                 <li><a href="#" className="hover:underline">Orders Tracking</a></li>
                                 <li><a href="#" className="hover:underline">Checkout</a></li>
@@ -39,18 +47,20 @@ export default function Footer() {
                             </ul>
                         </div>
                     </div>
-                    <div className="w-full lg:w-1/3 md:w-2/3 sm:w-full ">
+
+                    {/* Newsletter Section */}
+                    <div className="w-full lg:w-1/3 md:w-2/3 sm:w-full mb-8">
                         <div className="footer__newsletter">
-                            <h6 className="text-lg font-semibold mb-4">NEWSLETTER</h6>
-                            <form action="#" className="flex mb-4">
+                            <h6 className="text-lg font-semibold mb-4">Newsletter</h6>
+                            <form action="#" className="flex space-y-4 md:space-y-0">
                                 <input
                                     type="text"
                                     placeholder="Email"
-                                    className="px-4 py-2 w-2/3 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500"
+                                    className="px-4 py-2 w-full md:w-2/3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
                                 />
-                                <button type="submit" className="site-btn bg-red-500 text-white px-6 py-2">Subscribe</button>
+                                <button type="submit" className="site-btn bg-red-500 text-white px-6 py-2 w-full md:w-auto rounded-lg md:ml-2">Subscribe</button>
                             </form>
-                            <div className="footer__social flex space-x-4">
+                            <div className="footer__social flex space-x-4 mt-6">
                                 <a href="#" className="text-xl hover:text-gray-400"><i className="fa fa-facebook"></i></a>
                                 <a href="#" className="text-xl hover:text-gray-400"><i className="fa fa-twitter"></i></a>
                                 <a href="#" className="text-xl hover:text-gray-400"><i className="fa fa-youtube"></i></a>
@@ -59,11 +69,13 @@ export default function Footer() {
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
-            <div className="text-center mt-1 bg-gray-200 py-5">
-                <p className="text-gray-400 m-0">
-                    &copy; <script>document.write(new Date().getFullYear());</script>2024 All rights reserved | This ...
+
+            <div className="text-center mt-6 bg-gray-200 py-4">
+                <p className="text-gray-500">
+                    &copy; <script>document.write(new Date().getFullYear());</script> 2024 All rights reserved | This template is made ...
                 </p>
             </div>
         </footer>
